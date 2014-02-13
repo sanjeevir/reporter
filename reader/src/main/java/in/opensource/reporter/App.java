d1e15cf60480250f0440de4e139f8759d972c73a
sanjeevir <sanjeevi_nightwing@yahoo.com>
2014-02-13 08:21:38 +0530

    initial commit

---
+++ reader/src/main/java/in/opensource/reporter/App.java
@@ -0,0 +1,13 @@
+package in.opensource.reporter;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
